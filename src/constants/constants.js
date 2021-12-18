76d436988b6e0fa3d00d4bdc1d27a68cb91857f5
Geraldoeze <egerald344@gmail.com>
2021-12-18 21:02:31 +0100

    image

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,9 +2,8 @@ export const projects = [
   {
     title: 'Burger Application',
     description: "This is an application that lets you build a burger of your choice, you can add or remove some parts of the burger and it gets saved on a Firebase database",
-      image: '/images/1.png',
+      image: '/images/burger.png',
       tags: ['React', 'Firebase'],
-    source: 'https://google.com',
     visit: 'https://google.com',
     id: 0,
   },
@@ -13,8 +12,7 @@ export const projects = [
     description: "This applicationn fetches data from a database ",
     image: '/images/Robo_edited.jpg',
     tags: ['React', 'JavaScript', 'JSONplaceholder'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    visit: 'https://robo-frnd.herokuapp.com/',
     id: 1,
   },
   {
@@ -22,8 +20,7 @@ export const projects = [
     description: "A todo application helps in achieveing our daily goals, we can arrange our goals for the day in this application",
       image: '/images/todos.png',
       tags: ['React', 'JavaScript'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    visit: 'https://todos-prime.herokuapp.com/',
     id: 2,
   },
   {
@@ -31,8 +28,7 @@ export const projects = [
     description: "My first website with Next.js and React is a website thats stores users reciepe and ingredients",
     image: '/images/reciepe.png',
     tags: ['React', 'Next', 'JSONplaceholder'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    visit: 'https://reciepe.vercel.app/',
     id: 3,
   },
 ];
