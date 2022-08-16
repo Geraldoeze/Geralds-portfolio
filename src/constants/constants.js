7fc74065bdd0b4bb154eab0e57da4082d953546d
Gerald Ezenagu <egerald344@gmail.com>
2022-08-16 22:18:03 +0100

    removed old projects

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,11 +1,27 @@
 export const projects = [
   {
+    title: '',
+    description: '',
+    image: '',
+    tags: '',
+    visit: '',
+    id: 0
+  },
+  {
+    title: 'Gericht',
+    description: '',
+    image: '',
+    tags: '',
+    visit: 'https://gericht-app.vercel.app/',
+    id: 1
+  },
+  {
     title: 'Burger Application',
     description: "This is an application that lets you build a burger of your choice, you can add or remove some parts of the burger and it gets saved on a Firebase database",
       image: '/images/burger.png',
       tags: ['React.js', 'Firebase'],
     visit: 'https://burger-app-8ed4f.web.app',
-    id: 0,
+    id: 2,
   },
 
   {
@@ -14,9 +30,16 @@ export const projects = [
       image: '/images/hook.png',
       tags: ['React.js', 'Firebase'],
     visit: 'https://hooks-app.herokuapp.com/',
-    id: 1,
+    id: 3,
   },
-  
+  {
+    title: '',
+    description: '',
+    image: '',
+    tags: '',
+    visit: 'https://church-app-nu.vercel.app/',
+    id: 4
+  }
   
 ];
 
