b171a19db24e73c0154a17c6aa0547fe5e09286a
Gerald Ezenagu <egerald344@gmail.com>
2022-11-13 19:12:38 +0100

    update on contents

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,45 +1,45 @@
 export const projects = [
-  {
-    title: 'Realtor',
-    description: 'A Project built with Next.js that displays the information gotten from rapidAPI. It shows the places available for sale or for rent and information about the place. ',
-    image: '/images/realtor.png',
-    tags: ['Next.js', 'React.js', 'RapidAPI'],
-    visit: 'https://realtor-app-seven.vercel.app/',
-    id: 0
-  },
-  {
-    title: 'Gericht',
-    description: 'This is a restaurant landing page that shows the meals a restaurant offers to customers.',
-    image: '/images/gericht2.png',
-    tags: ['React.js'],
-    visit: 'https://gericht-app.vercel.app/',
-    id: 1
-  },
-  {
-    title: 'Burger Application',
-    description: "This is an application that lets you build a burger of your choice, you can add or remove some parts of the burger and it gets saved on a Firebase database",
-      image: '/images/burger.png',
-      tags: ['React.js', 'Firebase'],
-    visit: 'https://burger-app-8ed4f.web.app',
-    id: 2,
-  },
+  // {
+  //   title: 'Realtor',
+  //   description: 'A Project built with Next.js that displays the information gotten from rapidAPI. It shows the places available for sale or for rent and information about the place. ',
+  //   image: '/images/realtor.png',
+  //   tags: ['Next.js', 'React.js', 'RapidAPI'],
+  //   visit: 'https://realtor-app-seven.vercel.app/',
+  //   id: 0
+  // },
+  // {
+  //   title: 'Gericht',
+  //   description: 'This is a restaurant landing page that shows the meals a restaurant offers to customers.',
+  //   image: '/images/gericht2.png',
+  //   tags: ['React.js'],
+  //   visit: 'https://gericht-app.vercel.app/',
+  //   id: 1
+  // },
+  // {
+  //   title: 'Burger Application',
+  //   description: "This is an application that lets you build a burger of your choice, you can add or remove some parts of the burger and it gets saved on a Firebase database",
+  //     image: '/images/burger.png',
+  //     tags: ['React.js', 'Firebase'],
+  //   visit: 'https://burger-app-8ed4f.web.app',
+  //   id: 2,
+  // },
 
-  {
-    title: 'React Application',
-    description: "This React application stores items and amount in a Realtime Firebase Database, then gets the stored items and displays it on the browser DOM",
-      image: '/images/hook.png',
-      tags: ['React.js', 'Firebase'],
-    visit: 'https://hooks-app.herokuapp.com/',
-    id: 3,
-  },
-  {
-    title: 'Church App',
-    description: 'An appication that shows information about a church programs and evemts.',
-    image: '/images/church.png',
-    tags: ['React.js'],
-    visit: 'https://church-app-nu.vercel.app/',
-    id: 4
-  }
+  // {
+  //   title: 'React Application',
+  //   description: "This React application stores items and amount in a Realtime Firebase Database, then gets the stored items and displays it on the browser DOM",
+  //     image: '/images/hook.png',
+  //     tags: ['React.js', 'Firebase'],
+  //   visit: 'https://hooks-app.herokuapp.com/',
+  //   id: 3,
+  // },
+  // {
+  //   title: 'Church App',
+  //   description: 'An appication that shows information about a church programs and evemts.',
+  //   image: '/images/church.png',
+  //   tags: ['React.js'],
+  //   visit: 'https://church-app-nu.vercel.app/',
+  //   id: 4
+  // }
   
 ];
 
